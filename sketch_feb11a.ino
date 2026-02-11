81a5f50c8108eecdbb5dcad5221f0980ed89d440
elliottroach <elliott.roach@stu.ocsb.ca>
2026-02-11 14:48:42 -0500

    Add files via upload

---
+++ sketch_feb11a.ino
@@ -0,0 +1,18 @@
+// created by elliott roach
+// created on feb 2026
+// this blingks a light on and off
+
+
+void setup() {
+    // inisolizing pins
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop() {
+    // this blinks the light
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+}
