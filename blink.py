9a049073528f04f92ae5a85ec51acad5b57a5b32
elliottroach <elliott.roach@stu.ocsb.ca>
2026-02-19 13:07:02 -0500

    Update blink.py

--- blink.py
+++ blink.py
@@ -1,14 +1,18 @@
-# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
-
-"""Example for Pico. Turns on the built-in LED."""
+"""
+Made by Elliott Roach
+Made on 2026 feb
+this turns the light on and off
+"""
 import board
 import digitalio
+import time
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 while True:
     led.value = True
+    time.sleep(1)
+    led.value = False
+    time.sleep(1)
     
